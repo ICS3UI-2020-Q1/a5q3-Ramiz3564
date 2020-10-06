b70e330c6f902a2ccceb9246aa78915d4a9e64e6
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-10-06 15:28:25 +0000

    Finished Practice Q3 Oct 6

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+ import java.util.Scanner;
 /**
- *
- * @author 
+ * This program takes any number and computes the factorial of that number
+ * @author Zach Ramirez 
  */
 public class Main {
 
@@ -9,7 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input 
+    Scanner input = new Scanner(System.in); 
+    //ask user for an intger 
+    System.out.println("Please enter an integer to calculate the factorial of");
+    int integer = input.nextInt(); 
+    //creating a variable to keep count of expression
+    int count = integer; 
+    //the accumulator to add to the count into  
+    int sum = 1; 
+    //using loop to count up to the sum
+    while(count >= 1){
+      //print starting number
+      System.out.print(count + ",");
+      //adding the sum from 1 to whatever the user int is
+      sum = sum * count ; 
+      //increase value by 1
+      count = count - 1;
+    }
+    
+
+    //tell the user what the sum is 
+    System.out.println(integer + "! = " + sum); 
+
     
   }
 }
